fae359347cebbe7fc6b93f0acfc258eb2d6ccc0d
Alex Baranowski <alex@euro-linux.com>
2020-01-18 17:59:56 +0100

    els-ASAP auto commit file/tag perl-CPAN-Changes-0.20-1.el7.src.rpm

---
+++ perl-CPAN-Changes.spec
@@ -0,0 +1,109 @@
+Name:		perl-CPAN-Changes
+Summary:	Read and write Changes files
+Version:	0.20
+Release:	1%{?dist}
+License:	GPL+ or Artistic
+Group:		Development/Libraries
+URL:		http://search.cpan.org/dist/CPAN-Changes/
+Source0:	http://search.cpan.org/CPAN/authors/id/B/BR/BRICAS/CPAN-Changes-%{version}.tar.gz 
+Requires:	perl(:MODULE_COMPAT_%(eval "`perl -V:version`"; echo $version))
+BuildArch:	noarch
+BuildRequires:	perl >= 4:5.10.0
+BuildRequires:	perl(Cwd)
+BuildRequires:	perl(ExtUtils::MakeMaker)
+BuildRequires:	perl(File::Path)
+BuildRequires:	perl(Scalar::Util)
+BuildRequires:	perl(Test::More) >= 0.96
+BuildRequires:	perl(Test::Pod) >= 1.00
+BuildRequires:	perl(Test::Pod::Coverage) >= 1.00
+BuildRequires:	perl(Text::Wrap)
+BuildRequires:	perl(version) >= 0.79
+
+%description
+It is standard practice to include a Changes file in your distribution. The
+purpose of the Changes file is to help a user figure out what has changed
+since the last release.
+
+People have devised many ways to write the Changes file. A preliminary
+specification has been created (CPAN::Changes::Spec) to encourage module
+authors to write clear and concise Changes.
+
+This module will help users programmatically read and write Changes files
+that conform to the specification.
+
+%prep
+%setup -q -n CPAN-Changes-%{version}
+
+%build
+perl Makefile.PL INSTALLDIRS=vendor
+make %{?_smp_mflags}
+
+%install
+make pure_install DESTDIR=%{buildroot}
+find %{buildroot} -type f -name .packlist -exec rm -f {} \;
+%{_fixperms} %{buildroot}
+
+%check
+make test
+make test TEST_FILES="$(echo $(find xt/ -name '*.t'))"
+
+%files
+%doc Changes README
+%{_bindir}/tidy_changelog
+%{perl_vendorlib}/CPAN/
+%{perl_vendorlib}/Test/
+%{_mandir}/man1/tidy_changelog.1*
+%{_mandir}/man3/CPAN::Changes.3pm*
+%{_mandir}/man3/CPAN::Changes::Release.3pm*
+%{_mandir}/man3/CPAN::Changes::Spec.3pm*
+%{_mandir}/man3/Test::CPAN::Changes.3pm*
+
+%changelog
+* Thu May  2 2013 Paul Howarth <paul@city-fan.org> - 0.20-1
+- Update to 0.20:
+  - 'delete_empty_groups' shouldn't erronously delete default group
+  - Add tidy_changelog utility script
+  - Minor pod fix
+- Bump Test::More version requirement to 0.96 (CPAN RT#84994)
+
+* Thu Feb 14 2013 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 0.19-5
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_19_Mass_Rebuild
+
+* Tue Nov 13 2012 Jitka Plesnikova <jplesnik@redhat.com> - 0.19-4
+- Update dependencies
+
+* Fri Jul 20 2012 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 0.19-3
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_18_Mass_Rebuild
+
+* Tue Jun 12 2012 Petr Pisar <ppisar@redhat.com> - 0.19-2
+- Perl 5.16 rebuild
+
+* Tue May  1 2012 Paul Howarth <paul@city-fan.org> - 0.19-1
+- Update to 0.19:
+  - Test::CPAN::Changes now accepts version entries ending in '-TRIAL'
+    (CPAN RT#76882)
+  - releases() in CPAN::Changes also accepts entries ending in '-TRIAL'
+- Don't need to remove empty directories from buildroot
+- Drop %%defattr, redundant since rpm 4.4
+
+* Tue Jan 10 2012 Paul Howarth <paul@city-fan.org> - 0.18-2
+- Fedora 17 mass rebuild
+
+* Tue Oct 18 2011 Paul Howarth <paul@city-fan.org> - 0.18-1
+- Update to 0.18:
+  - Expand changes_file_ok() to accept arguments so that a specific version may
+    be checked
+  - Add $VERSION to Test::CPAN::Changes so it plays nice with the toolchain
+    e.g. Module::Install::AuthorRequires
+
+* Mon Jun 20 2011 Marcela Mašláňová <mmaslano@redhat.com> - 0.17-2
+- Perl mass rebuild
+
+* Thu Apr 21 2011 Paul Howarth <paul@city-fan.org> - 0.17-1
+- Update to 0.17:
+  - Eliminate extra whitespace when release data is not defined (CPAN RT#67441)
+  - Require version.pm 0.79, which introduced the $LAX regexp (CPAN RT#67613)
+  - Add the option to sort groups
+
+* Wed Apr 20 2011 Paul Howarth <paul@city-fan.org> - 0.16-1
+- Initial RPM version
---
+++ src/lib/CPAN/Changes/Release.pm
@@ -0,0 +1,254 @@
+package CPAN::Changes::Release;
+
+use strict;
+use warnings;
+
+sub new {
+    my $class = shift;
+    return bless {
+        changes => {},
+        @_,
+    }, $class;
+}
+
+sub version {
+    my $self = shift;
+
+    if ( @_ ) {
+        $self->{ version } = shift;
+    }
+
+    return $self->{ version };
+}
+
+sub date {
+    my $self = shift;
+
+    if ( @_ ) {
+        $self->{ date } = shift;
+    }
+
+    return $self->{ date };
+}
+
+sub changes {
+    my $self = shift;
+
+    if ( @_ ) {
+        my $group = shift;
+        return unless exists $self->{ changes }->{ $group };
+        return $self->{ changes }->{ $group };
+    }
+
+    return $self->{ changes };
+}
+
+sub add_changes {
+    my $self  = shift;
+    my $group = '';
+
+    if ( ref $_[ 0 ] ) {
+        $group = shift->{ group };
+    }
+
+    if ( !exists $self->{ changes }->{ $group } ) {
+        $self->{ changes }->{ $group } = [];
+    }
+
+    push @{ $self->{ changes }->{ $group } }, @_;
+}
+
+sub set_changes {
+    my $self  = shift;
+    my $group = '';
+
+    if ( ref $_[ 0 ] ) {
+        $group = shift->{ group };
+    }
+
+    $self->{ changes }->{ $group } = \@_;
+}
+
+sub clear_changes {
+    my $self = shift;
+    $self->{ changes } = {};
+}
+
+sub groups {
+    my $self = shift;
+    my %args = @_;
+
+    $args{ sort } ||= sub { sort @_ };
+
+    return $args{ sort }->( keys %{ $self->{ changes } } );
+}
+
+sub add_group {
+    my $self = shift;
+    $self->{ changes }->{ $_ } = [] for @_;
+}
+
+sub delete_group {
+    my $self   = shift;
+    my @groups = @_;
+
+    @groups = ( '' ) unless @groups;
+
+    delete $self->{ changes }->{ $_ } for @groups;
+}
+
+sub delete_empty_groups {
+    my $self = shift;
+
+    $self->delete_group($_) 
+        for grep { !@{ $self->changes( $_ ) } } $self->groups;
+}
+
+sub serialize {
+    my $self = shift;
+    my %args = @_;
+
+    my $output = join( ' ', grep { defined } ( $self->version, $self->date ) )
+        . "\n";
+
+    $output .= join "\n",
+        map { $self->_serialize_group( $_ ) }
+        $self->groups( sort => $args{ group_sort } );
+    $output .= "\n";
+
+    return $output;
+}
+
+sub _serialize_group {
+    my ( $self, $group ) = @_;
+
+    my $output = '';
+
+    $output .= sprintf " [%s]\n", $group if length $group;
+    $output .= Text::Wrap::wrap( ' - ', '   ', $_ ) . "\n"
+        for @{ $self->changes( $group ) };
+
+    return $output;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+CPAN::Changes::Release - Information about a particular release
+
+=head1 SYNOPSIS
+
+    my $rel = CPAN::Changes::Release->new(
+        version => '0.01',
+        date    => '2009-07-06',
+    );
+    
+    $rel->add_changes(
+        { group => 'THINGS THAT MAY BREAK YOUR CODE' },
+        'Return a Foo object instead of a Bar object in foobar()'
+    );
+
+=head1 DESCRIPTION
+
+A changelog is made up of one or more releases. This object provides access
+to all of the key data that embodies a release including the version number, 
+date of release, and all of the changelog information lines. Any number of 
+changelog lines can be grouped together under a heading.
+
+=head1 METHODS
+
+=head2 new( %args )
+
+Creates a new release object, using C<%args> as the default data.
+
+=head2 version( [ $version ] )
+
+Gets/sets the version number for this release.
+
+=head2 date( [ $date ] )
+
+Gets/sets the date for this release.
+
+=head2 changes( [ $group ] )
+
+Gets the list of changes for this release as a hashref of group/changes 
+pairs. If a group name is specified, an array ref of changes for that group 
+is returned. Should that group not exist, undef is returned.
+
+=head2 add_changes( [ \%options ], @changes )
+
+Appends a list of changes to the release. Specifying a C<group> option 
+appends them to that particular group. NB: the default group is represented 
+by and empty string.
+
+    # Append to default group
+    $release->add_changes( 'Added foo() function' );
+    
+    # Append to a particular group
+    $release->add_changes( { group => 'Fixes' }, 'Fixed foo() function' );
+
+=head2 set_changes( [ \%options ], @changes )
+
+Replaces the existing list of changes with the supplied values. Specifying
+a C<group> option will only replace change items in that group.
+
+=head2 clear_changes( )
+
+Clears all changes from the release.
+
+=head2 groups( sort => \&sorting_function )
+
+Returns a list of current groups in this release.
+
+If I<sort> is provided, groups are
+sorted according to the given function. If not,
+they are sorted alphabetically.
+
+=head2 add_group( @groups )
+
+Creates an empty group under the names provided.
+
+=head2 delete_group( @groups )
+
+Deletes the groups of changes specified.
+
+=head2 delete_empty_groups( )
+
+Deletes all groups that don't contain any changes.
+
+=head2 serialize( group_sort => \&sorting_function )
+
+Returns the release data as a string, suitable for inclusion in a Changes 
+file.
+
+If I<group_sort> is provided, change groups are
+sorted according to the given function. If not,
+groups are sorted alphabetically.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<CPAN::Changes::Spec>
+
+=item * L<CPAN::Changes>
+
+=item * L<Test::CPAN::Changes>
+
+=back
+
+=head1 AUTHOR
+
+Brian Cassidy E<lt>bricas@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2011-2013 by Brian Cassidy
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. 
+
+=cut
